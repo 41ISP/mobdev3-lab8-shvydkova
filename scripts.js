81250fb0bf006a3a78a47cdba2543945cf986725
Evakentei <eva.shvydkova.13@mail.ru>
2025-09-11 13:02:21 +0000

    commit_3

--- scripts.js
+++ scripts.js
@@ -1,23 +1,30 @@
 const songsContaner = document.querySelector(".tracks-list")
-//const statesContaner = document.querySelector(".stats")
-const artistsCon = document.querySelectorAll(".artists")
+const statesContaner = document.querySelector(".stats")
 async function fetchData() {
     const res1 = await fetch("https://kitek.ktkv.dev/songs.json")
     const res2 = await res1.json()
 
     for (let i = 0; i < res2.length; i++) {
+        const song = res2[i].track
+        const ListArt = song.album.artists
         const songsDiv = document.createElement("div")
         songsDiv.classList.add("song")
-        const spanSongs = document.createElement("h5")
-        const authorSongs = document.createElement("span")
-        spanSongs.textContent = res2[i].track.name
-        authorSongs.textContent = res2[i].track.album.artists.name;
-        
+        const spanSongs = document.createElement("h3")
+        spanSongs.textContent = song.name
         songsDiv.appendChild(spanSongs)
+
+        const authorSongs = document.createElement("span")
+        const authorNames = ListArt.map(artist => artist.name)
+        authorSongs.textContent = authorNames
         songsDiv.appendChild(authorSongs)
+
+
+        const albumSongs = document.createElement("h5")
+        albumSongs.textContent = song.album.name
+        songsDiv.appendChild(albumSongs)
         songsContaner.appendChild(songsDiv)
 
+        
     }
-
 }
 fetchData()
