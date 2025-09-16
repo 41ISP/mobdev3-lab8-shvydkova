a1904f3ee0897e623022732333762cc171255245
Evakentei <eva.shvydkova.13@mail.ru>
2025-09-16 05:50:36 +0000

    ne_pomnu

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 const songsContaner = document.querySelector(".tracks-list")
 const statesContaner = document.querySelector(".stats")
+const timeContaner = document.querySelector(".duration")
 async function fetchData() {
     const res1 = await fetch("https://kitek.ktkv.dev/songs.json")
     const res2 = await res1.json()
 
+    let runtime = 0
     for (let i = 0; i < res2.length; i++) {
         const song = res2[i].track
-        const ListArt = song.album.artists
+        runtime += song.album.duration_ms;
+        const ListArt = song.artists
         const songsDiv = document.createElement("div")
         songsDiv.classList.add("song")
         const spanSongs = document.createElement("h3")
@@ -24,7 +27,17 @@ async function fetchData() {
         songsDiv.appendChild(albumSongs)
         songsContaner.appendChild(songsDiv)
 
-        
+        const timeSong = document.createElement("div")
+        timeSong.classList.add("time")
+        const time = document.createElement("h3")
+        time.textContent = song.duration_ms;
+        timeSong.appendChild(time)
     }
+    console.log(runtime);
+    const string = document.createElement("div")
+    string.classList.add("stat")
+    string.textContent = "Треков: " + res2.length;
+    statesContaner.appendChild(string);
+
 }
 fetchData()
